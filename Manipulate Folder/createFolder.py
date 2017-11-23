93bf729ef669890801d99293bb8dda4628eedd70
singlino0619 <blackie1946.ec@i.softbank.jp>
2017-11-23 20:42:47 +0900

    revise comment out

--- Manipulate Folder/createFolder.py
+++ Manipulate Folder/createFolder.py
@@ -5,8 +5,10 @@ p_dir_path = os.getcwd() + "/" + sys.argv[1]
 # create parent-folder
 os.makedirs(p_dir_path)
 # create children-folder
+# the number of folders is determined by argv[3]
 for i in range(1, int(sys.argv[3]) + 1):
     # decide target path
+    # the children-folder name is created by the name of argv[2]
     targetpath = p_dir_path + "/" + sys.argv[2]
     # create children-folder in order
     os.makedirs(targetpath + str(i))
